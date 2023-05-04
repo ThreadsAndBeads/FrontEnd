43fd4aa7994754f35c428bda1d32ff3187002fd0
radwa nabil <radwaismail444@gmail.com>
2023-05-04 21:30:04 +0300

    sign up

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare const signIn: any;
+declare const signUp: any;
+declare const signInForm: any;
+declare const signUpForm: any;
+declare const overlay_container: any;
+declare const overlay: any;
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    
+    signIn.addEventListener("click", () => {
+      overlay_container.style.transform = "translateX(100%)";
+      overlay.style.transform = "translateX(-50%)";
+      signInForm.classList.add("active");
+      signUpForm.classList.remove("active");
+    });
+
+    signUp.addEventListener("click", () => {
+      overlay_container.style.transform = "translateX(0)";
+      overlay.style.transform = "translateX(0)";
+      signUpForm.classList.add("active");
+      signInForm.classList.remove("active");
+    });
+  }
 }
---
+++ src/assets/js/signup.js
@@ -0,0 +1,24 @@
+const signIn = document.querySelector("#signInButton");
+const signUp = document.querySelector("#signUpButton");
+const signInForm = document.querySelector(".container .sign-in-form");
+const signUpForm = document.querySelector(".container .sign-up-form");
+const overlay_container = document.querySelector(
+  ".container .overlay-container"
+);
+const overlay = document.querySelector(
+  ".container .overlay-container .overlay"
+);
+
+signIn.addEventListener("click", () => {
+  overlay_container.style.transform = "translateX(100%)";
+  overlay.style.transform = "translateX(-50%)";
+  signInForm.classList.add("active");
+  signUpForm.classList.remove("active");
+});
+signUp.addEventListener("click", () => {
+  overlay_container.style.transform = "translateX(0)";
+  overlay.style.transform = "translateX(0)";
+  signUpForm.classList.add("active");
+  signInForm.classList.remove("active");
+});
+
