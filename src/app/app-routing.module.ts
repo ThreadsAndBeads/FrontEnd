f97fb123b21ec906446ccd4f5d515a8d2e62a493
orabi55555 <elorabiahmed843@gmail.com>
2023-05-06 17:57:35 +0300

    about_us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HorizontallScrollComponent } from './components/horizontall-scroll/horizontall-scroll.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'about', component: AboutUsComponent },
+  {path:'/',component:HorizontallScrollComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HorizontallScrollComponent } from './components/horizontall-scroll/horizontall-scroll.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HorizontallScrollComponent
+    HorizontallScrollComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
