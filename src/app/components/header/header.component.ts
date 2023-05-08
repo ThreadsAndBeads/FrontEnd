078e090f49ab34dad4cd3f8e5ea425d6e9f76545
radwa nabil <radwaismail444@gmail.com>
2023-05-08 12:38:19 +0300

    intersection of navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,30 +1,19 @@
-import { Component, ViewChild, ElementRef, AfterViewInit ,Renderer2} from '@angular/core';
+// import { Component, ViewChild, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements AfterViewInit {
-  @ViewChild('navbar') navbar!: ElementRef;
-  constructor(private renderer: Renderer2) {}
-  ngAfterViewInit() {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting ) {
-    // Change background color of navbar
-    this.renderer.setStyle(this.navbar.nativeElement, 'background-color', 'red');
-  } else {
-    // Change background color of navbar back to default
-    this.renderer.setStyle(this.navbar.nativeElement, 'background-color', 'transparent');
-  }
-      });
-    }, {
-      root: null,
-      rootMargin: '-500px',
-      // threshold: 0
-    });
-
-    observer.observe(this.navbar.nativeElement);
+export class HeaderComponent {
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const navbar = document.querySelector('.navbar') as HTMLElement;
+    if (window.pageYOffset > navbar.clientHeight) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
   }
 }
