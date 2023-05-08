81f5dd312b65c57e4fa93b7ed87fe601b05677a3
Radwa Khalil <95037451+radwakhalil22@users.noreply.github.com>
2023-05-08 21:25:08 +0300

    top seller section

---
+++ src/app/components/top-sell/top-sell.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-sell',
+  templateUrl: './top-sell.component.html',
+  styleUrls: ['./top-sell.component.css']
+})
+export class TopSellComponent {
+
+}
