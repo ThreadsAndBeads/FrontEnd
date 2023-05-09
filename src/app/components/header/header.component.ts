6fd7b43968cdd5662260ff74d1a4381cd2ff5795
radwa nabil <radwaismail444@gmail.com>
2023-05-09 10:41:47 +0300

    check if user is logged path

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,8 @@
 // import { Component, ViewChild, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +10,20 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isUserLoggedIn: string|null = '';
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private tokenService: TokenStorageService,
+
+  ) {
+
+  }
+
+  public isUserLogged() {
+    return this.isUserLoggedIn = this.tokenService.getUserId();
+  }
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
     const navbar = document.querySelector('.navbar') as HTMLElement;
@@ -16,4 +33,5 @@ export class HeaderComponent {
       navbar.classList.remove('scrolled');
     }
   }
+
 }
