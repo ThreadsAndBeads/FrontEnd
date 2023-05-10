0e777f2d5e2b7849b7241123b35c6ad8755c9a16
Radwa Khalil <95037451+radwakhalil22@users.noreply.github.com>
2023-05-11 02:28:09 +0300

    workshop home section

---
+++ src/app/components/workshop-home-section/workshop-home-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workshop-home-section',
+  templateUrl: './workshop-home-section.component.html',
+  styleUrls: ['./workshop-home-section.component.css']
+})
+export class WorkshopHomeSectionComponent {
+
+}
