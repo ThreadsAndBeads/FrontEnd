5d9a428081aba5c7118b5ff41870eabd192efd5e
Hager Khaled <78926069+hagerk720@users.noreply.github.com>
2023-05-12 22:33:41 +0300

    first shot on search

--- src/app/model/product.model.ts
+++ src/app/model/product.model.ts
@@ -2,7 +2,7 @@ import { FileHandle } from "./file-handler.model";
 
 export interface Product {
 
-  
+    _id?: string | null, 
     user_id: string | null,
     name : string ,
     description : string ,
