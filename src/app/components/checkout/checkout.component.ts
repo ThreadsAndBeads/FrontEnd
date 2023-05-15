6d4c2382427a6770d5d7f180c6273d7f469f6680
marim99 <marim.khaled99@gmail.com>
2023-05-16 00:57:50 +0300

    checkout

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,7 +1,7 @@
 import { formatDate } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Order } from 'src/app/model/order.model';
 import { CartService } from 'src/app/services/cart.service';
@@ -14,18 +14,35 @@ import { TokenStorageService } from 'src/app/services/token-storage.service';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent {
+  // @ViewChild('myModal') myModal!: ElementRef;
   userId = this.tokenService.getUser()._id ;
+  validationForm!: FormGroup;
+  payment_method:any;
   cartProducts!: any;
   totalItems!: number;
   totalPrice!: number;
+  error = {
+    client_name: '',
+    phone: '',
+    city: '',
+    address: '',
+  };
   client=this.tokenService.getUser();
-  constructor(private orderService : OrderService ,private router:Router,private tokenService: TokenStorageService,public cartService: CartService)
+  constructor(private orderService : OrderService ,private router:Router,private tokenService: TokenStorageService,public cartService: CartService,  private fb: FormBuilder)
    { 
+     this.validationForm = this.fb.group({
+       client_name: [this.client.name, [Validators.required]],
+       phone: [this.client.phone, [Validators.required]],
+       city:['', Validators.required],
+       address:['', Validators.required],
+       // payment_method:['']
+     });
     this.checkCartStatus();
+    console.log(this.validationForm);
+
   }
-
-
   checkCartStatus() {
+    if (this.validationForm.valid) {
     this.cartService.getCartProducts().subscribe({
       next: (response) => {
         if (response.data.cart.products) {
@@ -41,6 +58,7 @@ export class CheckoutComponent {
       },
     });
   }
+}
 
   calculateSubtotal() {
     this.totalItems = 0;
@@ -51,16 +69,32 @@ export class CheckoutComponent {
       this.totalPrice += product.quantity * product.productId.price;
     });
   }
-  createOrder(client_name:string, phone:string, address:string,payment_method:string){
+  get client_name() {
+    return this.validationForm.get('client_name');
+  }
+  get phone() {
+    return this.validationForm.get('phone');
+  }
+  get city() {
+    return this.validationForm.get('city');
+  }
+
+  get address() {
+    return this.validationForm.get('address');
+  }
+  createOrder(){
     // console.log(this.userId);
-    const order ={client_name,phone,address,payment_method}; 
-    const newOrder=this.prepareFormData(order);
+    if (this.validationForm.valid) {
+      
+    const order ={address:this.address!.value,client_name:this.client_name!.value,phone:this.phone!.value,city:this.city!.value}; 
+    console.log(order);
+    
+    const newOrder=this.prepareOrder(order);
     console.log(newOrder);
     
     this.orderService.createOrder(newOrder).subscribe({
      next: (res : any) =>{
-        // console.log(response);
-        // orderForm.reset();
+ 
         this.router.navigate(['/home']);
         
       },
@@ -69,35 +103,41 @@ export class CheckoutComponent {
       }
   });
   }
-  prepareFormData(order: any){
-    const orderDate=formatDate(new Date(), 'yyyy/MM/dd', 'en');
+}
+  paymentMethod(e:any) {
+    this.payment_method= e.target.value;
+ }
+  prepareOrder(order: any){
     let newOrder = {
       userId: this.userId,
       client_name: order.client_name,
-      address: order.address,
+      clientAddress: {
+        apartmentNo: order.apartmentNo || null,
+        city: order.city || null,
+        address: order.address || null,
+      },
       phone: order.phone,
-      payment_method: order.payment_method ,
-      totalPrice:  this.totalPrice,
-      orderDate:orderDate,
-      orderStatus:'pending',
-      products:this.cartProducts
+      payment_method: this.payment_method ,
     };
-   // formData.append('phone', user.phone.toString());
     return newOrder;
   }
 
-  validationForm = new FormGroup({
-    client_name:new FormControl(this.client.name),
-    phone:new FormControl(this.client.phone),
-    city:new FormControl(null),
-    ZIP:new FormControl(null),
-    flat:new FormControl(null),
-    address:new FormControl(null),
-    payment_method:new FormControl(null),
-  })
 
-  get NameValid(){
-    return this.validationForm.controls["client_name"].valid;
-  }
 
+  // get NameValid(){
+  //   return this.validationForm.controls["client_name"].valid;
+  // }
+  // get PhoneValid(){
+  //   return this.validationForm.controls["phone"].valid;
+  // }
+  // get AddressValid(){
+  //   // return this.validationForm.controls["address"].valid;
+  // }
+  // get CityValid(){
+  //   return this.validationForm.controls["city"].valid;
+  // }
+
+  // get payment_method() {
+  //   return this.validationForm.get('payment_method');
+  // }
 }
